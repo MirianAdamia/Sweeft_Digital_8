8d9b60db36484df961b1c95dd770375297959507
Mirian Adamia <m.adamia@Imedil.ge>
2022-07-15 00:01:10 +0400

    Add project files.

---
+++ Sweeft_Digital_8/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Sweeft_Digital_8.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Sweeft_Digital_8.Controllers
+{
+    [Controller]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpPost("DownloadCountries")]       
+        public async Task<IActionResult> DownloadCountries()
+        {
+            using (var client = _httpClientFactory.CreateClient())
+            {
+                var httpRequest = new HttpRequestMessage(HttpMethod.Get, "https://restcountries.com/v3.1/all");
+
+                using (var response = await client.SendAsync(httpRequest))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+
+                    IEnumerable<Country> countries
+                        = JsonConvert.DeserializeObject<IEnumerable<Country>>(apiResponse);
+
+                    var lines = new List<string>();
+
+                    foreach(var country in countries)
+                    {
+                        var line = $"Region - {country.Region}, Subregion - {country.Subregion}, " +
+                            $"Population - {country.Population}, Area - {country.Area}, Latlng - ";
+
+                        foreach (var lat in country.Latlng)
+                            line += $" {lat}";
+
+                        lines.Add(line);
+                    }
+
+                    await System.IO.File.WriteAllLinesAsync("Countries.txt", lines);
+
+                    return Ok(countries);
+                }
+            }
+        }
+       
+
+    }
+}
---
+++ Sweeft_Digital_8/Models/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Sweeft_Digital_8.Models
+{
+    public class Country
+    {
+        public string Region { get; set; }
+        public string Subregion { get; set; }
+        public List<decimal> Latlng { get; set; }
+        public decimal Area { get; set; }
+        public int Population { get; set; }
+    }
+}
